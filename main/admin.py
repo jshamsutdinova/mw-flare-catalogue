85d4f67db9f15a00d6bd18c8bc8316c55bf1f06e
jshamsutdinova <shjn0297@gmail.com>
2024-12-02 14:34:00 +0800

    Changed the view of admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from .models import Team, Publication, Conference, Presentation, Proceedings
 
-# Register your models here.
+
 admin.site.register(Team)
 admin.site.register(Publication)
-admin.site.register(Conference)
-admin.site.register(Presentation)
+
+@admin.register(Conference)
+class ConferenceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Presentation)
+class PresentatationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type', 'conf', 'first_author']
+    list_filter = ['type', 'first_author', 'conf']
+
+
 admin.site.register(Proceedings)
