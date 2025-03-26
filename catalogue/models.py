f8c0a547eff15b476823c57d94b34cf2fb101f1e
jshamsutdinova <shjn0297@gmail.com>
2025-03-26 15:02:40 +0800

    Added tag field. Tag was assigned by the CNN model

--- catalogue/models.py
+++ catalogue/models.py
@@ -25,12 +25,21 @@ class FlareList(models.Model):
     
    
 class Flare(models.Model):
+    
+    class Tag(models.IntegerChoices):
+        FLARE = 0, 'flare'
+        BACKGROUND = 1, 'background'
+        ARTIFACT = 2, 'artifact'
+
     date = models.ForeignKey(FlareList, on_delete=models.CASCADE)
     start_event = models.TimeField()
     maximum_event = models.TimeField()
     finish_event = models.TimeField()
     min_freq = models.FloatField()
     max_freq = models.FloatField()
+    tag = models.IntegerField(default=Tag.FLARE, choices=Tag.choices)
+
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         title = f"{self.date.date.strftime("%Y-%m-%d")} \
