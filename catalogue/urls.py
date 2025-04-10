23977ddbc8d3ec562ae3d9a03f4ee4d18cd354ee
jshamsutdinova <shjn0297@gmail.com>
2025-04-10 18:33:52 +0800

    Changed structure of canonical urls

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -7,5 +7,5 @@ app_name = "catalogue"
 
 urlpatterns = [
     path("", views.month_flare_list, name='month_flare_list'),
-    path('<int:dt>/', views.flare_list, name='flare_list'),
+    path('<int:year>/<int:month>/<int:day>/', views.flare_list, name='flare_list'),
 ]
