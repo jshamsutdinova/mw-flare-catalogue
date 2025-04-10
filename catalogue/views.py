311adfbc89dfce8a75e1f26dcc48591136ee5476
jshamsutdinova <shjn0297@gmail.com>
2025-04-10 18:37:17 +0800

    Added filter to get total flares. Added objects with queryset to get url

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.utils.dates import MONTHS
+from django.db.models import Count
 
-from catalogue.models import FlareList, Flare
+from catalogue.models import Flare
 from .forms import YearMonthForm
 
 
@@ -12,17 +12,38 @@ def month_flare_list(request):
         if form.is_valid():
             year = form.cleaned_data['year']
             month = form.cleaned_data['month']
-            records = FlareList.objects.filter(date__year=year, date__month=month)
+            flare_counts = Flare.objects.filter(
+                date__year=year, date__month=month).values('date').annotate(Count('date')).order_by()
+            
+            objects = []
+            for rec in flare_counts:
+                obj = Flare.objects.filter(date=rec['date'])[0]
+                objects.append(obj)
+
             return render(request, 'catalogue/form.html',  {'form': form,
-                                                            'records': records,
+                                                            'records': flare_counts,
+                                                            'objects': objects,
                                                             'year': year,
                                                             'month': MONTHS[int(month)]})
     else:
-        userform = YearMonthForm()
+        userform = YearMonthForm() 
         return render(request, 'catalogue/form.html', {'form': userform})
+ 
 
-def flare_list(request, dt):
-    list = get_object_or_404(FlareList, date=str(dt))
-    flares = Flare.objects.filter(date__date=str(dt))
-    return render(request, "catalogue/flare/day_list.html", {'list': list,
-                                                             'flares': flares})
+def flare_list(request, year, month, day):
+    flares = Flare.objects.filter(date__year=year,
+                                  date__month=month,
+                                  date__day=day)
+    return render(request, "catalogue/flare/day_list.html", {'flares': flares})
+
+
+def delete_artifacts(request):
+    if request.method == 'POST':
+        del_toggle = request.POST.get('del_toggle', False)
+
+        if del_toggle:
+            flare_list = Flare.objects.filter(tag=0)
+        else:
+            flare_list = Flare.objects.all()
+        
+    return render(request, 'catalogue/form.html', {'flare_list': flare_list})
