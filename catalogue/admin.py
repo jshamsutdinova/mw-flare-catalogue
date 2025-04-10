d843dc0c47bfffe0deee8308a8149340d04eedee
jshamsutdinova <shjn0297@gmail.com>
2025-04-10 18:28:16 +0800

    Redesigned the database. Deleted FlareList model

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from catalogue.models import FlareList, Flare
-
-
-@admin.register(FlareList)
-class FlareListAdmin(admin.ModelAdmin):
-    list_display  = ['date', 'date_added']
-    search_fields = ['date']
-    ordering = ['-date']
+from catalogue.models import Flare
 
 
 @admin.register(Flare)
 class FlareAdmin(admin.ModelAdmin):
-    list_display = ['date', 'start_event', 'finish_event', 'updated']
+    list_display = ['date', 'start_event', 'end_event', 'updated']
     list_filter  = ['tag']
--- catalogue/models.py
+++ catalogue/models.py
@@ -3,27 +3,6 @@ from django.db import models
 from django.urls import reverse
 
 
-class FlareList(models.Model):
-    date = models.DateField()
-    num_flare = models.PositiveIntegerField(null=True)
-    diagram = models.ImageField(upload_to="img/", null=True)
-    csv_file = models.FileField(upload_to="csv/", null=True)
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['date']
-
-    def __str__(self):
-        return self.date.strftime("%Y-%m-%d")
-    
-    def get_absolute_url(self):
-        dt = self.date.strftime("%Y%m%d")
-        return reverse("catalogue:flare_list", args=[dt])
-    
-    def filename(self):
-        return os.path.basename(self.csv_file.name)
-    
-   
 class Flare(models.Model):
     
     class Tag(models.IntegerChoices):
@@ -31,17 +10,26 @@ class Flare(models.Model):
         BACKGROUND = 1, 'background'
         ARTIFACT = 2, 'artifact'
 
-    date = models.ForeignKey(FlareList, on_delete=models.CASCADE)
-    start_event = models.TimeField()
-    maximum_event = models.TimeField()
-    finish_event = models.TimeField()
-    min_freq = models.FloatField()
-    max_freq = models.FloatField()
+    date = models.DateField()
+    start_event = models.DateTimeField()
+    maximum_event = models.DateTimeField()
+    end_event = models.DateTimeField(null=True)
+    min_freq  = models.FloatField()
+    max_freq  = models.FloatField()
     tag = models.IntegerField(default=Tag.FLARE, choices=Tag.choices)
 
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        title = f"{self.date.date.strftime("%Y-%m-%d")} \
-            {self.start_event.strftime("%H:%M:%S")}-{self.finish_event.strftime("%H:%M:%S")}"
+        title = f"{self.date.strftime("%Y-%m-%d")} \
+            {self.start_event.strftime("%H:%M:%S")}-{self.end_event.strftime("%H:%M:%S")}"
         return title
+    
+    def get_absolute_url(self):
+        dt = self.date.strftime("%Y%m%d")
+        return reverse("catalogue:flare_list",
+                       args=[
+                           self.date.year,
+                           self.date.month,
+                           self.date.day
+                       ])
